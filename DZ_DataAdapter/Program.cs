0ae08050d8a7cbf7100d9e47c493ba041199c086
Stytan <stytanus@gmail.com>
2017-08-31 19:29:06 +0300

    first

--- DZ_DataAdapter/Program.cs
+++ DZ_DataAdapter/Program.cs
@@ -1,22 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+/*
+ * Created by SharpDevelop.
+ * User: sergey.lezhenko
+ * Date: 31.08.2017
+ * Time: 9:10
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
 using System.Windows.Forms;
 
 namespace DZ_DataAdapter
 {
-    static class Program
-    {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-        }
-    }
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
 }
